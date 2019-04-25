a21d0ddb48c89f9afa069d52e3962bfccc797893
George-Taxiarchou <drdarky@hotmail.com>
2019-04-25 14:19:06 +0300

    window query success

--- windowquerie.py
+++ windowquerie.py
@@ -36,16 +36,14 @@ def getCellPos(x,y,limitsarray):
     cellY =int( yposition )
     return [cellX,cellY,xposition,yposition]
 
-def windowQ(x_low,x_high,y_low,y_high,limitsarray):
+def windowQ(x_low,x_high,y_low,y_high,limitsarray,cellmatrix,pointsmatrix):
+    queryanswer = []
+
     if(x_low>x_high):
         print "Error x_low>x_high"
         exit(0)
     elif(y_low>y_high):
         print "Error y_low>y_high"
-        exit(0)
-
-    if(x_low<limitsarray[0] or x_high>limitsarray[1] or y_low<limitsarray[2] or y_high>limitsarray[3]):
-        print "Error , query out of grid bounds"
         exit(0)
 
     firstcell = getCellPos(x_low,y_low,limitsarray)
@@ -56,36 +54,64 @@ def windowQ(x_low,x_high,y_low,y_high,limitsarray):
     cellsmaxX = secondcell[2]
     cellsmaxY = secondcell[3]
 
-    print '\n'
-    print [cellsminX,cellsmaxX,cellsminY,cellsmaxY]
-    print "\n"
-
-
     intersectingcellsarray = []
     for i in range(firstcell[0],secondcell[0]+1):
         for j in range(firstcell[1],secondcell[1]+1):
-            intersectingcellsarray.append([i,j])
+            if(i<10 and j<10 and i>=0 and j>=0):
+                intersectingcellsarray.append([i,j])
 
     for cell in intersectingcellsarray:
-        print cell[0] , cell[1]
+        # print cell[0] , cell[1]
         if(cell[0]>=cellsminX and cell[0]+1<=cellsmaxX and cell[1]>=cellsminY and cell[1]+1<=cellsmaxY):
             cell.append("full")
+        else:
+            cell.append("check")
 
-    return intersectingcellsarray
+    numberofpointsadded = 0
+    for cell in intersectingcellsarray:
+        for cellmatrixcell in cellmatrix:
+            if(cellmatrixcell[0]==cell[0] and cellmatrixcell[1]==cell[1]):
+                if(cell[2]=="full"):
+                    for i in range(cellmatrixcell[3]):
+                        queryanswer.append(pointsmatrix[cellmatrixcell[2]+i])
+                        numberofpointsadded+=1
+                elif(cell[2]=="check"):
+                    for i in range(cellmatrixcell[3]):
+                        point = pointsmatrix[cellmatrixcell[2]+i]
+                        pointposition = getCellPos(point[1],point[2],limitsarray)
+                        pointx = pointposition[2]
+                        pointy = pointposition[3]
+                        if(pointx>=cellsminX and pointx<=cellsmaxX and pointy>=cellsminY and pointy<=cellsmaxY):
+                            queryanswer.append(point)
+                            numberofpointsadded+=1
 
-
+                #AESTHETIC PRINTS START
+    print "--------------------------------"
+    for point in queryanswer:
+        print point
+    print "--------------------------------"
+    print "--------------------------------------"
+    for cell in intersectingcellsarray:
+        if(cell[2]=="full"):
+            print "Completely covered by window : " +"["+ cell[0].__str__()+","+cell[1].__str__() +"]"
+        elif(cell[2]=="check"):
+            print "Partially covered by window : "+"["+ cell[0].__str__()+","+cell[1].__str__() +"]"
+    print "--------------------------------------"
+    print "-----------------------------"
+    print "WindowX_low : "+cellsminX.__str__()+"\nWindowX_high : "+cellsmaxX.__str__()+"\nWindowY_low : "+cellsminY.__str__()+"\nWindowY_max : "+cellsmaxY.__str__()
+    print "-----------------------------"+"\n"+"-----------------------------"
+    print "  Points in Window : " + numberofpointsadded.__str__()
+    print "-----------------------------"
+                #END OF PRINTS
 def main():
     griddir = open("grid.dir","r")
     gridgrd = open("grid.grd","r")
-    # beijing = open('Beijing_restaurants.txt',"r")
 
     cellparser = scan(griddir,"dir")
     pointparser = scan(gridgrd,"grd")
-    # beijingparser = scan(beijing,"dir")
 
     cellmatrix = []
     pointsmatrix = []
-    # beijingmatrix = []
 
     numberofcells = file_len("grid.dir")
     numberofpoints = file_len("grid.grd")
@@ -102,9 +128,8 @@ def main():
 
     griddir.close()
     gridgrd.close()
-
-    print windowQ(39.68009,39.772154,116.070466,116.425554,limitsarray)
-
-
+    
+    # windowQ(39.68009,40.179911,116.070466,116.719976,limitsarray,cellmatrix,pointsmatrix)
+    windowQ(39.68009,39.772154,116.070466,116.425554,limitsarray,cellmatrix,pointsmatrix)
 
 main()
